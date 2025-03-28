b5e156e91719c81643106d7764f819ffdb932a52
anyone-bit <uniqueyouand@gmail.com>
2025-03-28 18:22:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function appendValue(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculateResult() {
+    document.getElementById("display").value = eval(document.getElementById("display").value);
+}
